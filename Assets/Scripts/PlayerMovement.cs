37b8087fe48a07fb63ad9c08cdc0b0e5742270dc
Fulfinsen <dincasilviu2@gmail.com>
2024-02-17 11:32:45 +0200

    Adding jump and modified the background o abit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,31 +7,53 @@ using UnityEngine.InputSystem;
 public class NewBehaviourScript : MonoBehaviour
 {
 
+    [SerializeField] public float speed;
+    [SerializeField] public float jumpSpeed;
+
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
-    [SerializeField]public float speed;
+    Animator myAnimator;
+    CapsuleCollider2D capsuleCollider;
+    
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         Run();
         FlipSprite();
-    }
+    }   
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
+        }
     }
 
     void Run()
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * speed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
     }
 
     void FlipSprite()
