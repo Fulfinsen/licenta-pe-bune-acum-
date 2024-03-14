351396931e80362d95b7e960a14ad90e99f92307
Fulfinsen <dincasilviu2@gmail.com>
2024-03-14 22:12:58 +0200

    Menu section

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void playGame ()
+    {
+        SceneManager.LoadScene("Level 1");
+        Debug.Log("Start!!!");
+    }
+
+    public void quit()
+    {
+        Debug.Log("Quit!!!");
+        Application.Quit();
+    }
+}
